d2a0d7445d3d340969587fa081ac8c24f7b3e0f1
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-15 15:07:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200815150627_create_appointments.rb
+++ db/migrate/20200815150627_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.appointment_datetime :datetime
-      t.user :belongs_to
-      t.doctor :belongs_to
+      t.belongs_to :doctor
+      t.belongs_to :patient
 
       t.timestamps
     end
